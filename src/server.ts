7465dc2a677f7744149cad2d8624fca92e398d08
y-v-h <gyrinovich@gmail.com>
2026-03-19 21:49:04 +0100

    feat: add server start script

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-const test = () => {};
+import 'dotenv/config';
+import fastify from './app.js';
+
+const PORT = Number(process.env.PORT) || 4000;
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
