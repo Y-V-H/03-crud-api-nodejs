02bc227e1e49914f7de5e5fd03a70fa8ef9c6757
y-v-h <gyrinovich@gmail.com>
2026-03-20 22:42:39 +0100

    feat: add get method by product id

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,23 +1,33 @@
 import type { FastifyInstance } from 'fastify';
-// import { uuidValidate } from 'node';
 import { db } from '../db/index.js';
 import { z } from 'zod';
+
+interface Params {
+  productId: string;
+}
 
 async function routes(fastify: FastifyInstance, options: unknown) {
   fastify.get('/api/products', async () => {
     return db;
   });
-  // fastify.get('/api/products:productID', async (request, reply) => {
-  //   const { productID } = request.params;
-  //   const isValidUUID = z.string().uuid().safeParse(productID).success;
-  //   if (isValidUUID) {
-  //     const requestedProduct = db.filter((el) => el.id === productID);
-  //     return requestedProduct;
-  //   } else {
-  //     return;
-  //   }
-  //   return db;
-  // });
+
+  fastify.get<{
+    Params: Params;
+  }>('/api/products/:productId', async (request, reply) => {
+    const { productId } = request.params;
+    const isValidUUID = z.uuid().safeParse(productId).success;
+
+    if (isValidUUID) {
+      const product = db.find((el) => el.id === productId);
+      if (product) {
+        return product;
+      } else {
+        reply.status(404).send({ message: 'Product not found' });
+      }
+    } else {
+      reply.status(400).send({ message: 'Invalid product ID' });
+    }
+  });
 }
 
 export { routes };
--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const productSchema = z.object({
+const productSchema = z.object({
   name: z.string(),
   description: z.string(),
   price: z.number().positive(),
