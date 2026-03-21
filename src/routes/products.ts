62e827c0b4f8d96ce3b10c7a10ac5e96f9526576
y-v-h <gyrinovich@gmail.com>
2026-03-21 13:17:47 +0100

    feat: add PUT method

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -47,6 +47,35 @@ async function routes(fastify: FastifyInstance, options: unknown) {
       reply.status(201).send(newProduct);
     }
   });
+
+  fastify.put<{
+    Params: Params;
+  }>('/api/products/:productId', async (request, reply) => {
+    const { productId } = request.params;
+    const isValidUUID = z.uuid().safeParse(productId).success;
+    const result = productSchema.safeParse(request.body);
+
+    if (!isValidUUID) {
+      reply.status(400).send({ message: 'Invalid product ID' });
+      return;
+    }
+
+    if (result.success) {
+      const product = db.find((el) => el.id === productId);
+      if (product) {
+        const index = db.findIndex((el) => el.id === productId);
+
+        db[index] = { ...result.data, id: productId };
+
+        reply.status(201).send(db[index]);
+      } else {
+        reply.status(404).send({ message: 'Product not found' });
+      }
+    } else {
+      reply.status(400).send({ message: 'Invalid request body' });
+      return;
+    }
+  });
 }
 
 export { routes };
